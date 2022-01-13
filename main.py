90fbe30bd4bb249b8bfeb276268a1a72347f593a
kerelMblsh <dorovskih811@gmail.com>
2022-01-13 22:55:56 +0700

    Добавил файлы сохранения и меню загрузки, обновил фон главного меню

--- main.py
+++ main.py
@@ -3,26 +3,24 @@ import sys
 
 import pygame
 import pygame_gui
-
-from select_lvl import select_level_menu
+import csv
+import sqlite3
 
 
 pygame.init()
 
 FPS = 30
 SIZE = WIDTH, HEIGHT = 800, 600
-COLOR = "black"
 SOUND_VOLUME = 0.75
+CUR_SAVE = "save_1"
+LEVEL_OPENED = 1
+
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption("Битва за Салонце")
 
 clock = pygame.time.Clock()
 
 manager = pygame_gui.UIManager(SIZE)
-
-pygame.mixer.music.load("data/sounds/menu.wav")
-pygame.mixer.music.set_volume(0.75)
-pygame.mixer.music.play(-1)
 
 
 def terminate():
@@ -68,6 +66,10 @@ def kill_elements(elems):
 
 
 def start_menu():
+    pygame.mixer.music.load("data/sounds/menu.wav")
+    pygame.mixer.music.set_volume(0.75)
+    pygame.mixer.music.play(-1)
+
     screen.fill(pygame.Color("black"))
     start_logo = load_image("start_bg.jpg")
     screen.blit(start_logo, (0, 0))
@@ -110,7 +112,7 @@ def start_menu():
                 if event.ui_element == start_game_btn:
                     select_level_menu()
                 elif event.ui_element == upload_game_btn:
-                    upload_game_menu()
+                    upload_game_menu("load")
                 elif event.ui_element == stat_btn:
                     stat_menu()
                 elif event.ui_element == settings_btn:
@@ -127,7 +129,107 @@ def start_menu():
         pygame.display.update()
 
 
-def upload_game_menu():
+def upload_game_menu(mode):
+    global CUR_SAVE
+    global LEVEL_OPENED
+
+    bg = load_image("start_bg_without_logo.jpg")
+    screen.blit(bg, (0, 0))
+
+    load_1 = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((50, 20), (300, 80)),
+        text='Слот сохранения 1', manager=manager
+    )
+    load_2 = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((50, 120), (300, 80)),
+        text='Слот сохранения 2', manager=manager
+    )
+    load_3 = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((50, 220), (300, 80)),
+        text='Слот сохранения 3', manager=manager
+    )
+    load_4 = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((50, 320), (300, 80)),
+        text='Слот сохранения 4', manager=manager
+    )
+    load_5 = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((50, 420), (300, 80)),
+        text='Слот сохранения 5', manager=manager
+    )
+    load_6 = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((450, 20), (300, 80)),
+        text='Слот сохранения 6', manager=manager
+    )
+    load_7 = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((450, 120), (300, 80)),
+        text='Слот сохранения 7', manager=manager
+    )
+    load_8 = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((450, 220), (300, 80)),
+        text='Слот сохранения 8', manager=manager
+    )
+    load_9 = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((450, 320), (300, 80)),
+        text='Слот сохранения 9', manager=manager
+    )
+    load_10 = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((450, 420), (300, 80)),
+        text='Слот сохранения 10', manager=manager
+    )
+    exit_to_menu_btn = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((250, 520), (300, 60)),
+        text='Вернуться в меню', manager=manager
+    )
+    elements = [load_1, load_2, load_3, load_4, load_5,
+                load_6, load_7, load_8, load_9, load_10, exit_to_menu_btn]
+    save_to_file = {load_1: 'save_1',
+                    load_2: 'save_2',
+                    load_3: 'save_3',
+                    load_4: 'save_4',
+                    load_5: 'save_5',
+                    load_6: 'save_6',
+                    load_7: 'save_7',
+                    load_8: 'save_8',
+                    load_9: 'save_9',
+                    load_10: 'save_10'}
+    temp_save = "save_1"
+    while True:
+        time_delta = clock.tick(60) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element in elements:
+                    load_sound("button_clicked.mp3")
+                    if event.ui_element != exit_to_menu_btn:
+                        temp_save = save_to_file[event.ui_element]
+                        confirmation_dialog = pygame_gui.windows.UIConfirmationDialog(
+                            rect=pygame.Rect((250, 200), (300, 200)),
+                            manager=manager,
+                            window_title="Подтверждение",
+                            action_long_desc=f"Загрузить сохранение {temp_save.split('_')[1]}?",
+                            action_short_name='OK',
+                            blocking=True
+                        )
+                if event.ui_element == exit_to_menu_btn:
+                    kill_elements(elements)
+                    return
+            if event.type == pygame_gui.UI_BUTTON_ON_HOVERED:
+                if event.ui_element in elements:
+                    load_sound("button_hover_2.mp3")
+            if event.type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
+                CUR_SAVE = temp_save
+                with open(f'data/saves/{CUR_SAVE}.csv', encoding="utf8") as csvfile:
+                    if mode == "load":
+                        LEVEL_OPENED = list(csv.reader(csvfile, delimiter=';', quotechar='"'))[1][1]
+            manager.process_events(event)
+        manager.update(time_delta)
+        screen.blit(bg, (0, 0))
+        manager.draw_ui(screen)
+        pygame.display.update()
+
+
+def select_level_menu():
     pass
 
 
@@ -135,7 +237,6 @@ def parameters():
     global SOUND_VOLUME
 
     bg = load_image("start_bg_without_logo.jpg")
-    screen.fill((0, 0, 0))
     screen.blit(bg, (0, 0))
     label_1 = pygame_gui.elements.UILabel(
         relative_rect=pygame.Rect((25, 25), (250, 50)),
@@ -166,8 +267,8 @@ def parameters():
         text='Вернуться в меню',
         manager=manager
     )
-    settings_elements = [label_1, label_2, music_volume_scr_bar,
-                         sound_volume_scr_bar, exit_to_menu_btn]
+    elements = [label_1, label_2, music_volume_scr_bar,
+                sound_volume_scr_bar, exit_to_menu_btn]
     while True:
         music_volume = music_volume_scr_bar.get_current_value()
         SOUND_VOLUME = sound_volume_scr_bar.get_current_value() / 100
@@ -180,7 +281,7 @@ def parameters():
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == exit_to_menu_btn:
                     load_sound("button_clicked.mp3")
-                    kill_elements(settings_elements)
+                    kill_elements(elements)
                     return
             if event.type == pygame_gui.UI_BUTTON_ON_HOVERED:
                 if event.ui_element == exit_to_menu_btn:
