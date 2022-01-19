035a7b7b889eac427a73f2656a285faf34f2d240
kerelMblsh <dorovskih811@gmail.com>
2022-01-19 15:23:02 +0700

    Добавлен индикатор жизней, батарея и убийство противников

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import os
 import sys
 
 import pygame
 import pygame_gui
 import csv
-import sqlite3
 import random
 
 
-from sprites import ALL_SPRITES, PLAYER_BULLET, ALIEN_SPRITES, ALIEN_BULLET_SPRITES
-from sprites import Background, MainCharacter, Enemy, Boss, MainCharBullet, AlienBullet
+from sprites import ALL_SPRITES, PLAYER_BULLET, ALIEN_SPRITES, ALIEN_BULLET_SPRITES, BONUSES, HEARTS
+from sprites import Background, MainCharacter, Enemy, Boss, MainCharBullet, AlienBullet, Heart, Battery
+from sprites import load_image
 
 
 pygame.init()
@@ -18,9 +17,6 @@ SIZE = WIDTH, HEIGHT = 800, 600
 MUSIC_VOLUME = SOUND_VOLUME = 0.5
 CUR_SAVE = "save_1"
 LEVEL_OPENED = 1
-
-con = sqlite3.connect("data/game_data.db")
-cur = con.cursor()
 
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption("Битва за Салонце")
@@ -33,21 +29,6 @@ manager = pygame_gui.UIManager(SIZE)
 def terminate():
     pygame.quit()
     sys.exit()
-
-
-def load_image(name, color_key=None):
-    fullname = os.path.join('data/images', name)
-    try:
-        image = pygame.image.load(fullname)
-    except pygame.error as message:
-        print('Не удаётся загрузить:', name)
-        raise SystemExit(message)
-    image = image.convert_alpha()
-    if color_key is not None:
-        if color_key == -1:
-            color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key)
-    return image
 
 
 def load_sound(name):
@@ -214,14 +195,13 @@ def select_level_menu():
                      mors_im, kemlya_im, veneda_im, merciry_im, salonce_im]
     planet_images_coords = [(50, 50), (50, 200), (50, 350), (185, 50), (200, 200),
                             (200, 350), (350, 50), (350, 200), (350, 350), (515, 325)]
-
+    choosen_planet = "Плитон"
     planet_choose = pygame_gui.elements.ui_drop_down_menu.UIDropDownMenu(
         options_list=planets,
-        starting_option=planets[LEVEL_OPENED - 1],
+        starting_option=choosen_planet,
         relative_rect=pygame.Rect((500, 50), (200, 50)),
         manager=manager
     )
-    choosen_planet = "Плитон"
     start_level = pygame_gui.elements.UIButton(
         relative_rect=pygame.Rect((60, 500), (310, 60)),
         text='Запустить уровень',
@@ -349,7 +329,7 @@ def upload_game_menu(mode):
                     if event.ui_element != exit_to_menu_btn:
                         temp_save = save_to_file[event.ui_element]
                         pygame_gui.windows.UIConfirmationDialog(
-                            rect=pygame.Rect((250, 200), (300, 150)),
+                            rect=pygame.Rect((250, 200), (300, 200)),
                             manager=manager,
                             window_title="Подтверждение",
                             action_long_desc=f"Загрузить сохранение {temp_save.split('_')[1]}?",
@@ -501,30 +481,36 @@ def stat_menu():
 
 def game_process(level):
     load_music("battle.wav")
+    clock.tick(120)
     # характеристики уровней: название файла фона, радиус выбора времени спавна врагов,
-    # убитых врагов для появления босса, кол-во hp у врагов, кол-во hp у босса
+    # убитых врагов для появления босса, кол-во hp у врагов, кол-во hp у босса,
+    # время на восстановление 1 заряда батареи
     level_parameters = {
-                        "Плитон": ["pliton_bg.png", (1000, 3000), 10, 50, 1000],
-                        "Тептун": ["teptune_bg.png", (900, 2500), 12, 50, 1250],
-                        "Буран": ["buran_bg.png", (800, 2000), 15, 50, 1500],
-                        "Матурн": ["maturne_bg.png", (700, 1700), 17, 50, 1750],
-                        "Юпатер": ["jupater_bg.png", (650, 1500), 20, 50, 2000],
-                        "Морс": ["mors_bg.png", (600, 1300), 22, 100, 2250],
-                        "Кемля": ["kemlya_bg.png", (550, 1100), 25, 100, 2500],
-                        "Венеда": ["veneda_bg.png", (500, 1000), 27, 100, 2750],
-                        "Меркирий": ["merciry_bg.png", (450, 900), 30, 100, 3000],
-                        "Салонце (финал)": ["salonce_bg.png", (400, 800), 40, 100, 5000]
+                        "Плитон": ["pliton_bg.png", (1000, 3000), 10, 50, 1000, 1000],
+                        "Тептун": ["teptune_bg.png", (900, 2500), 12, 50, 1250, 930],
+                        "Буран": ["buran_bg.png", (800, 2000), 15, 50, 1500, 860],
+                        "Матурн": ["maturne_bg.png", (700, 1700), 17, 50, 1750, 790],
+                        "Юпатер": ["jupater_bg.png", (650, 1500), 20, 50, 2000, 720],
+                        "Морс": ["mors_bg.png", (600, 1300), 22, 100, 2250, 650],
+                        "Кемля": ["kemlya_bg.png", (550, 1100), 25, 100, 2500, 580],
+                        "Венеда": ["veneda_bg.png", (500, 1000), 27, 100, 2750, 510],
+                        "Меркирий": ["merciry_bg.png", (450, 900), 30, 100, 3000, 440],
+                        "Салонце (финал)": ["salonce_bg.png", (400, 800), 40, 150, 5000, 370]
                         }
     enemy_ic_elems = {"enemy_ic_loop_1_16.png": 16,
                       "enemy_ic_loop_2_6.png": 6,
                       "enemy_ic_loop_3_4.png": 4}
     cur_lvl_par = level_parameters[level]
     Background(load_image(cur_lvl_par[0]), ALL_SPRITES)
-    main_char = MainCharacter(load_image("new_hero_ic_loop_hit_2.png"), 4, 30, 255, 8, [102, 43], ALL_SPRITES)
+    main_char = MainCharacter("new_hero_ic_loop_hit_2.png", 4, 30, 255, 10, [102, 43], ALL_SPRITES)
+    Heart(main_char, 0, (5, 5), ALL_SPRITES, HEARTS)
+    Heart(main_char, 50, (40, 5), ALL_SPRITES, HEARTS)
+    Heart(main_char, 100, (75, 5), ALL_SPRITES, HEARTS)
+    bat = Battery(cur_lvl_par[5], (5, 541), ALL_SPRITES)
     enemies = []
     is_back_to_menu = False
     is_next_lvl = False
-    start_ticks = pygame.time.get_ticks()
+    enemy_spawn_ticks = pygame.time.get_ticks()
     time_distance = random.randint(*cur_lvl_par[1])
     points = 0
     player_shots = 0
@@ -554,10 +540,12 @@ def game_process(level):
                 if event.key == pygame.K_ESCAPE:
                     is_back_to_menu = pause_menu()
                 if event.key == pygame.K_f:
-                    load_sound("shot_sound.wav")
-                    MainCharBullet(load_image("bullet_player.png"), 4,
-                                   *main_char.shot_bullet_pos, 15, ALL_SPRITES, PLAYER_BULLET)
-                    player_shots += 1
+                    if bat.charge > 0:
+                        load_sound("shot_sound.wav")
+                        MainCharBullet("bullet_player.png", 4,
+                                       *main_char.shot_bullet_pos, 15, ALL_SPRITES, PLAYER_BULLET)
+                        player_shots += 1
+                        bat.charge -= 1
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_UP:
                     main_char.move_to_up = False
@@ -567,30 +555,41 @@ def game_process(level):
                     main_char.move_to_left = False
                 if event.key == pygame.K_RIGHT:
                     main_char.move_to_right = False
-        if time_distance < (pygame.time.get_ticks() - start_ticks):
+        if time_distance < (pygame.time.get_ticks() - enemy_spawn_ticks):
             rand_enemy_ic_name = random.choice(["enemy_ic_loop_1_16.png",
                                                 "enemy_ic_loop_2_6.png",
                                                 "enemy_ic_loop_3_4.png"])
             enemy_ic = load_image(rand_enemy_ic_name)
-            enemy = Enemy(enemy_ic, enemy_ic_elems[rand_enemy_ic_name], 800,
+            enemy = Enemy(rand_enemy_ic_name, enemy_ic_elems[rand_enemy_ic_name], 800,
                           random.randint(0, 600 - enemy_ic.get_height()),
                           random.randint(8, 16), [-20, enemy_ic.get_height() // 2 - 2],
                           cur_lvl_par[3], ALL_SPRITES, ALIEN_SPRITES)
             enemies.append(enemy)
             time_distance = random.randint(*cur_lvl_par[1])
-            start_ticks = pygame.time.get_ticks()
-        if main_char.hp <= 0:
-            is_next_lvl, is_back_to_menu = game_end(level, False, cur_lvl_par[2], points,
-                                                    player_shots, killed_enemies)
+            enemy_spawn_ticks = pygame.time.get_ticks()
+        alive_enemies = []
+        for enemy in enemies:
+            if not enemy.alive():
+                killed_enemies += 1
+            else:
+                alive_enemies.append(enemy)
+        enemies = alive_enemies
+        if main_char.is_dead:
+            if main_char.alive():
+                main_char.kill()
+                death_time = pygame.time.get_ticks()
+            if pygame.time.get_ticks() - death_time > 1250:
+                is_next_lvl, is_back_to_menu = game_end(level, False, points,
+                                                        player_shots, killed_enemies)
         if False:  # босс убит
-            is_next_lvl, is_back_to_menu = game_end(level, True, cur_lvl_par[2], points,
+            is_next_lvl, is_back_to_menu = game_end(level, True, points,
                                                     player_shots, killed_enemies)
         ALL_SPRITES.draw(screen)
         ALL_SPRITES.update()
         pygame.display.update()
 
 
-def game_end(level, is_player_won, need_to_kill_value,
+def game_end(level, is_player_won,
              player_points, player_shots, killed_enemies):
     load_music("level_end.mp3")
     if is_player_won:
@@ -601,7 +600,7 @@ def game_end(level, is_player_won, need_to_kill_value,
 
     label_1 = pygame_gui.elements.UILabel(
         relative_rect=pygame.Rect((40, 175), (340, 90)),
-        text=f"Убитых врагов: {killed_enemies} из {need_to_kill_value}",
+        text=f"Убитых врагов: {killed_enemies}",
         manager=manager
     )
     label_2 = pygame_gui.elements.UILabel(
@@ -666,19 +665,19 @@ def game_end(level, is_player_won, need_to_kill_value,
 
 
 def pause_menu():
-    bg = load_image("start_bg_without_logo.jpg")
+    bg = load_image("pause_bg.jpg")
     screen.blit(bg, (0, 0))
 
     back_to_game_btn = pygame_gui.elements.UIButton(
-        relative_rect=pygame.Rect((250, 160), (300, 80)),
+        relative_rect=pygame.Rect((220, 175), (340, 90)),
         text="Вернуться в игру", manager=manager
     )
     settings_btn = pygame_gui.elements.UIButton(
-        relative_rect=pygame.Rect((250, 255), (300, 80)),
+        relative_rect=pygame.Rect((220, 280), (340, 90)),
         text="Параметры", manager=manager
     )
     exit_to_menu_btn = pygame_gui.elements.UIButton(
-        relative_rect=pygame.Rect((250, 350), (300, 80)),
+        relative_rect=pygame.Rect((220, 385), (340, 90)),
         text="Выйти в меню", manager=manager
     )
     elements = [back_to_game_btn, settings_btn, exit_to_menu_btn]
--- sprites.py
+++ sprites.py
@@ -1,9 +1,12 @@
 import os
 import pygame
+import random
 
 
 ALL_SPRITES = pygame.sprite.Group()
 PLAYER_BULLET = pygame.sprite.Group()
+BONUSES = pygame.sprite.Group()
+HEARTS = pygame.sprite.Group()
 ALIEN_SPRITES = pygame.sprite.Group()
 ALIEN_BULLET_SPRITES = pygame.sprite.Group()
 clock = pygame.time.Clock()
@@ -42,10 +45,11 @@ class Ship(pygame.sprite.Sprite):
         super().__init__(*group)
         self.pos = [x, y]
         self.v = v
+        self.start_sheet = sheet
         self.start_bullet_pos = start_bullet_pos
         self.set_image(sheet, columns, *self.pos)
         self.mask = pygame.mask.from_surface(self.image)
-
+        self.start_ticks = pygame.time.get_ticks()
         self.shot_bullet_pos = [self.pos[0] + start_bullet_pos[0],
                                 self.pos[1] + start_bullet_pos[1]]
         self.add(ALL_SPRITES)
@@ -60,7 +64,7 @@ class Ship(pygame.sprite.Sprite):
 
     def set_image(self, sheet, columns, x, y):
         self.frames = []
-        self.cut_sheet(sheet, columns)
+        self.cut_sheet(load_image(sheet), columns)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
         self.rect = self.image.get_rect().move(x, y)
@@ -82,55 +86,68 @@ class MainCharacter(Ship):
         self.move_to_right = False
         self.hp = 150
         self.harmless = True  # неуязвимость после удара
-        self.start_ticks = pygame.time.get_ticks()
+        self.is_almost_dead = False
+        self.is_dead = False
         self.add(ALL_SPRITES)
 
     def update(self):
         self.cur_frame = (self.cur_frame + 1) % len(self.frames)
         self.image = self.frames[self.cur_frame]
         self.mask = pygame.mask.from_surface(self.image)
-        self.shot_bullet_pos = [self.pos[0] + self.start_bullet_pos[0],
-                                self.pos[1] + self.start_bullet_pos[1]]
-        if self.move_to_up:
-            self.pos[1] -= self.v
-            self.rect = self.rect.move((0, -self.v))
-        if self.move_to_down:
-            self.pos[1] += self.v
-            self.rect = self.rect.move((0, self.v))
-        if self.move_to_left:
-            self.pos[0] -= self.v
-            self.rect = self.rect.move((-self.v, 0))
-        if self.move_to_right:
-            self.pos[0] += self.v
-            self.rect = self.rect.move((self.v, 0))
-        if self.harmless:
-            if pygame.time.get_ticks() - self.start_ticks > 2000:
-                self.set_image(load_image("new_hero_ic_loop.png"), 4, *self.pos)
-                self.harmless = False
-        ship_collide = pygame.sprite.spritecollideany(self, ALIEN_SPRITES)
-        if ship_collide:
-            print(ship_collide.groups())
-            if pygame.sprite.collide_mask(self, ship_collide):
-                if not self.harmless:
-                    self.get_hit()
-        bullet_collide = pygame.sprite.spritecollideany(self, ALIEN_BULLET_SPRITES)
-        if bullet_collide:
-            if pygame.sprite.collide_mask(self, bullet_collide):
-                if not self.harmless:
-                    self.get_hit()
+        if not self.is_almost_dead:
+            self.shot_bullet_pos = [self.pos[0] + self.start_bullet_pos[0],
+                                    self.pos[1] + self.start_bullet_pos[1]]
+            if self.move_to_up and self.pos[1] >= 0:
+                self.pos[1] -= self.v
+                self.rect = self.rect.move((0, -self.v))
+            if self.move_to_down and self.pos[1] <= 600 - self.image.get_height():
+                self.pos[1] += self.v
+                self.rect = self.rect.move((0, self.v))
+            if self.move_to_left and self.pos[0] >= 0:
+                self.pos[0] -= self.v
+                self.rect = self.rect.move((-self.v, 0))
+            if self.move_to_right and self.pos[0] <= 800 - self.image.get_width():
+                self.pos[0] += self.v
+                self.rect = self.rect.move((self.v, 0))
+            if self.hp <= 0:
+                self.is_almost_dead = True
+                self.set_image("new_hero_ic_loop_death.png", 7, *self.pos)
+            if self.harmless:
+                if pygame.time.get_ticks() - self.start_ticks > 2000:
+                    self.set_image("new_hero_ic_loop.png", 4, *self.pos)
+                    self.harmless = False
+            ship_collide = pygame.sprite.spritecollideany(self, ALIEN_SPRITES)
+            if ship_collide:
+                if pygame.sprite.collide_mask(self, ship_collide):
+                    if not self.harmless:
+                        self.get_hit()
+            bullet_collide = pygame.sprite.spritecollideany(self, ALIEN_BULLET_SPRITES)
+            if bullet_collide:
+                if pygame.sprite.collide_mask(self, bullet_collide):
+                    if not self.harmless:
+                        self.get_hit()
+        else:
+            if self.cur_frame == 6:
+                self.is_dead = True
 
     def get_hit(self):
         self.hp -= 50
         self.start_ticks = pygame.time.get_ticks()
         self.harmless = True
-        self.set_image(load_image("new_hero_ic_loop_hit.png"), 4, *self.pos)
+        self.set_image("new_hero_ic_loop_hit_2.png", 4, *self.pos)
 
 
 class Enemy(Ship):
     def __init__(self, sheet, columns, x, y, v, start_bullet_pos, hp, *group):
         super().__init__(sheet, columns, x, y, v, start_bullet_pos, *group)
+        self.enemy_death_im = {"enemy_ic_loop_1_16.png": "enemy_ic_1_death.png",
+                               "enemy_ic_loop_2_6.png": "enemy_ic_2_death.png",
+                               "enemy_ic_loop_3_4.png": "enemy_ic_3_death.png"}
         self.hp = hp
         self.add(ALIEN_SPRITES)
+        self.shot_time_dist = random.randint(700, 1500)
+        self.is_almost_dead = False
+        self.is_dead = False
 
     def update(self):
         self.cur_frame = (self.cur_frame + 1) % len(self.frames)
@@ -140,8 +157,25 @@ class Enemy(Ship):
                                 self.pos[1] + self.start_bullet_pos[1]]
         self.pos[0] -= self.v
         self.rect = self.rect.move((-self.v, 0))
+        if pygame.time.get_ticks() - self.start_ticks > self.shot_time_dist:
+            self.shot()
+        bullet_collide = pygame.sprite.spritecollideany(self, PLAYER_BULLET)
+        if bullet_collide:
+            self.hp -= 50
+            bullet_collide.kill()
+        if self.hp <= 0 and not self.is_almost_dead:
+            self.is_almost_dead = True
+            self.set_image(self.enemy_death_im[self.start_sheet], 5, *self.pos)
+        if self.is_almost_dead and self.cur_frame == 4:
+            self.kill()
         if self.pos[0] < -100:
             self.kill()
+
+    def shot(self):
+        AlienBullet("bullet_alien.png", 4, *self.shot_bullet_pos,
+                    self.v * 2, ALL_SPRITES, ALIEN_BULLET_SPRITES)
+        self.shot_time_dist = random.randint(700, 1500)
+        self.start_ticks = pygame.time.get_ticks()
 
 
 class Boss(Enemy):
@@ -152,7 +186,7 @@ class Bullet(pygame.sprite.Sprite):
     def __init__(self, sheet, columns, x, y, v, *group):
         super().__init__(*group)
         self.frames = []
-        self.cut_sheet(sheet, columns)
+        self.cut_sheet(load_image(sheet), columns)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
         self.rect = self.rect.move(x, y)
@@ -198,3 +232,43 @@ class MainCharBullet(Bullet):
         self.rect = self.rect.move((self.v, 0))
         if self.pos[0] > 900:
             self.kill()
+
+
+class Heart(pygame.sprite.Sprite):
+    def __init__(self, char, hp_to_disable, pos, *group):
+        super().__init__(*group)
+        self.char = char
+        self.hp_to_disable = hp_to_disable
+        self.image = load_image("full_heart.png")
+        self.enabled = True
+        self.rect = self.image.get_rect().move(pos)
+        self.pos = pos
+
+    def update(self):
+        if self.hp_to_disable >= self.char.hp and self.enabled:
+            self.image = load_image("empty_heart.png")
+            self.enabled = False
+        elif self.hp_to_disable < self.char.hp and not self.enabled:
+            self.image = load_image("full_heart.png")
+            self.enabled = True
+
+
+class Battery(pygame.sprite.Sprite):
+    def __init__(self, inc_charge_time_dist, pos, *group):
+        super().__init__(*group)
+        self.inc_charge_time_dist = inc_charge_time_dist
+        self.images = []
+        for i in range(11):
+            self.images.append(load_image(f"battery_charge/battery_charge_{i}.png"))
+        self.charge = 10
+        self.image = self.images[self.charge]
+        self.rect = self.image.get_rect().move(pos)
+        self.pos = pos
+        self.time_to_inc_charge = pygame.time.get_ticks()
+
+    def update(self):
+        if pygame.time.get_ticks() - self.time_to_inc_charge > self.inc_charge_time_dist and \
+                self.charge < 10:
+            self.charge += 1
+            self.time_to_inc_charge = pygame.time.get_ticks()
+        self.image = self.images[self.charge]
